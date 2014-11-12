cf2c7c0b22047760228d4192dc6111e40c2a4b67
RUSSCITY <ruslan.ragimov.rwth@gmail.com>
2014-11-12 11:18:19 +0100

    - parameter renamed

--- src/main/java/com/bitstars/meta/updater/MetaUpdater.java
+++ src/main/java/com/bitstars/meta/updater/MetaUpdater.java
@@ -29,18 +29,18 @@ public class MetaUpdater {
 	 * Updates object o1 with attributes of object jo2 depends on meta model of
 	 * o1
 	 *
-	 * @param o1
-	 * @param jo2
+	 * @param target
+	 * @param updatedObject
 	 * @return
 	 * @throws UpdaterException
 	 */
-	public <T> T updateObject(T o1, JSONObject jo2) throws UpdaterException {
-		MetaModel mm = new MetaParser().getMetaAsObject(o1);
-		JSONObject jo1 = new DataParser().parseSingleObject(o1);
+	public <T> T updateObject(T target, JSONObject updatedObject) throws UpdaterException {
+		MetaModel mm = new MetaParser().getMetaAsObject(target);
+		JSONObject jo1 = new DataParser().parseSingleObject(target);
 
-		JSONObject result = updateObject(mm, jo1, jo2);
+		JSONObject result = updateObject(mm, jo1, updatedObject);
 
-		T newOb = (T) new Gson().fromJson(jo1.toString(), o1.getClass());
+		T newOb = (T) new Gson().fromJson(jo1.toString(), target.getClass());
 		return newOb;
 	}
 
@@ -49,14 +49,14 @@ public class MetaUpdater {
 	 * mm
 	 *
 	 * @param mm
-	 * @param jo1
-	 * @param jo2
+	 * @param target
+	 * @param updatedObject
 	 * @return
 	 * @throws UpdaterException
 	 */
-	public JSONObject updateObject(MetaModel mm, JSONObject jo1, JSONObject jo2)
+	public JSONObject updateObject(MetaModel mm, JSONObject target, JSONObject updatedObject)
 			throws UpdaterException {
-		Iterator<?> keys = jo1.keys();
+		Iterator<?> keys = target.keys();
 
 		// Iterate over attributes of original object
 		while (keys.hasNext()) {
@@ -67,10 +67,10 @@ public class MetaUpdater {
 					&& !mm.getTYPE_ID().equals(key)) {
 
 				// Check fields NOT_NULL
-				if (jo2.has(key)) {
-					if ((jo2.get(key) == null || jo2.get(key).toString()
+				if (updatedObject.has(key)) {
+					if ((updatedObject.get(key) == null || updatedObject.get(key).toString()
 							.equals(""))
-							&& (jo1.get(key) != null || !jo1.get(key)
+							&& (target.get(key) != null || !target.get(key)
 									.toString().equals(""))
 							&& mm.getFIELDS_NOT_NULL().contains(key)) {
 						throw new UpdaterException("Can not update field '"
@@ -92,10 +92,10 @@ public class MetaUpdater {
 									.getATTRIBUTE_TYPE()
 									.equals(MetaJSONTranslator.ATTRIBUTE_TYPE_SINGLE_STR)) {
 						complex = true;
-						jo1.put(key,
+						target.put(key,
 								updateObject(complexField.getMETA_DATA(),
-										jo1.getJSONObject(key),
-										jo2.getJSONObject(key)));
+										target.getJSONObject(key),
+										updatedObject.getJSONObject(key)));
 					}
 				}
 
@@ -103,21 +103,21 @@ public class MetaUpdater {
 					// check if field has a regex
 					for (Map<String, String> map : mm.getFIELDS_REGEX()) {
 						if (map.containsKey(key)) {
-							if (!checkRegex(jo2.get(key).toString(),
+							if (!checkRegex(updatedObject.get(key).toString(),
 									map.get(key))) {
 								throw new UpdaterException("Key '" + key
 										+ "' has a regex '" + map.get(key)
 										+ "' which is not matched by value '"
-										+ jo2.get(key) + "'");
+										+ updatedObject.get(key) + "'");
 							}
 						}
 					}
-					jo1.put(key, jo2.get(key));
+					target.put(key, updatedObject.get(key));
 				}
 
 			}
 		}
-		return jo1;
+		return target;
 	}
 
 	/**
